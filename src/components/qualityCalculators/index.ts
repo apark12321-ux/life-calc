c41146be88c7651704ac35737767a7550613eef9
apark12321-ux <apark12321@gmail.com>
2026-06-23 08:01:00 +0900

    Connect expanded Korean calculator catalog

--- src/components/qualityCalculators/index.ts
+++ src/components/qualityCalculators/index.ts
@@ -1,7 +1,13 @@
 import { koreaEssentialCalculators } from './catalogKoreaEssential';
+import { koreaExpansionCalculators } from './catalogKoreaExpansion';
+import { calculate as baseCalculate, defaultValues } from './logic';
+import { calculateExpansion } from './logicExpansion';
+import { CalculatorSpec, Values } from './model';
 
-export const calculatorCatalog = koreaEssentialCalculators;
+export const calculatorCatalog = [...koreaEssentialCalculators, ...koreaExpansionCalculators];
 
-export { calculate, defaultValues } from './logic';
+export const calculate = (spec: CalculatorSpec, values: Values) => calculateExpansion(spec, values) ?? baseCalculate(spec, values);
+
+export { defaultValues };
 export { categoryKeys, categoryLabels } from './model';
 export type { CalcCategory, CalculatorSpec, Field, ResultRow, Values } from './model';
